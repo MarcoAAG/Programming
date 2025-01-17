1f935e3c81a63e77495a5c5e68d831c3abc86339
MarcoAAG <marco-antonioa@outlook.com>
2025-01-16 21:35:33 -0600

    add ll

---
+++ ds/linkedlist/ll.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+typedef struct 
+{
+  int value;
+  int *next;
+}t_node;
+
+// Create List
+// Create node
+// append
+// prepend
+// printList
+// Insert new node
+// Get Lenght
+// Delete node
+// free memory
+
+int main()
+{
+  return 0;
+}
